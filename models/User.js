0c73d046da2d13bd3057eb0e0b19b311dcdf71eb
chaudeveloper <ms_michanguyen@yahoo.com>
2022-04-02 01:33:41 -0400

    add logics to models, routes and controllers

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    userName: {
+      type: String,
+      unique: true, 
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, 
+      trim: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+      }
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+  );
+
+  UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+  
+
+
+const User = model('User', UserSchema);
+
+
+module.exports = User;
